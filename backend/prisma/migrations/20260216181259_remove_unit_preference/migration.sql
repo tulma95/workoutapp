56f3a45f82ea5569996434eb7141005432a6afd4
Riku <>
2026-02-16 20:13:23 +0200

    migration: drop unit_preference column from users

---
+++ backend/prisma/migrations/20260216181259_remove_unit_preference/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `unit_preference` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "unit_preference";
